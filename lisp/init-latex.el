086ca49aff48e4e7881a09a0867006738f1de564
sdll <shura.sdll@gmail.com>
2016-11-13 22:17:08 +0000

    added latex editing customizations

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -118,4 +118,57 @@
 ;; Further pdf support
 
 (pdf-tools-install)
+
+;; Enable outline-minor-mode for folding
+
+(add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+
+;; Outline-minor-mode key map
+(define-prefix-command 'cm-map nil "Outline-")
+                                        ; HIDE
+(define-key cm-map "q" 'hide-sublevels)    ; Hide everything but the top-level headings
+(define-key cm-map "t" 'hide-body)         ; Hide everything but headings (all body lines)
+(define-key cm-map "o" 'hide-other)        ; Hide other branches
+(define-key cm-map "c" 'hide-entry)        ; Hide this entry's body
+(define-key cm-map "l" 'hide-leaves)       ; Hide body lines in this entry and sub-entries
+(define-key cm-map "d" 'hide-subtree)      ; Hide everything in this entry and sub-entries
+                                        ; SHOW
+(define-key cm-map "a" 'show-all)          ; Show (expand) everything
+(define-key cm-map "e" 'show-entry)        ; Show this heading's body
+(define-key cm-map "i" 'show-children)     ; Show this heading's immediate child sub-headings
+(define-key cm-map "k" 'show-branches)     ; Show all sub-headings under this heading
+(define-key cm-map "s" 'show-subtree)      ; Show (expand) everything in this heading & below
+                                        ; MOVE
+(define-key cm-map "u" 'outline-up-heading)                ; Up
+(define-key cm-map "n" 'outline-next-visible-heading)      ; Next
+(define-key cm-map "p" 'outline-previous-visible-heading)  ; Previous
+(define-key cm-map "f" 'outline-forward-same-level)        ; Forward - same level
+(define-key cm-map "b" 'outline-backward-same-level)       ; Backward - same level
+(global-set-key (kbd "C-'") cm-map)
+
+;; make math mode easier to enter
+
+
+
+(add-hook 'plain-TeX-mode-hook
+          (lambda () (set (make-variable-buffer-local 'TeX-electric-math)
+                     (cons "$" "$"))))
+(add-hook 'LaTeX-mode-hook
+          (lambda () (set (make-variable-buffer-local 'TeX-electric-math)
+                     (cons "\\(" "\\)"))))
+;; use zathura for pdfs
+
+(custom-set-variables
+ ;; Your other configuration here
+ ;; ...
+ '(TeX-view-program-list (quote (("Zathura" "zathura %o")))) ; [1]
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks) "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Zathura")                                    ; [2]
+     (output-html "xdg-open")))))
+(setq TeX-electric-sub-and-superscript t)
+
+(setq LaTeX-electric-left-right-brace nil)
 (provide 'init-latex)
