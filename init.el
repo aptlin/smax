1d452d58f5aa85d5c8ab822ee11c893bd8ac273f
sdll <shura.sdll@gmail.com>
2016-11-14 01:41:52 +0000

    added support for w3m

--- init.el
+++ init.el
@@ -52,6 +52,8 @@
 (require-package 'mwe-log-commands)
 
 (require 'init-gnus)
+(require 'init-browser)
+(require 'init-eshell)
 (require 'init-frame-hooks)
 ;;(require 'init-xterm)
 (require 'init-themes)
---
+++ lisp/init-browser.el
@@ -0,0 +1,32 @@
+;;From
+;;http://beatofthegeek.com/2014/02/my-setup-for-using-emacs-as-web-browser.html
+
+;;change default browser for 'browse-url' to w3m
+(require-package 'w3m)
+(setq browse-url-browser-function 'w3m-goto-url-new-session)
+(setq w3m-default-display-inline-images t)
+(defun wikipedia-search (search-term)
+  "Search for SEARCH-TERM on wikipedia"
+  (interactive
+   (let ((term (if mark-active
+                   (buffer-substring (region-beginning) (region-end))
+                 (word-at-point))))
+     (list
+      (read-string
+       (format "Wikipedia (%s):" term) nil nil term)))
+   )
+  (browse-url
+   (concat
+    "http://en.m.wikipedia.org/w/index.php?search="
+    search-term
+    ))
+  )
+
+;;when I want to enter the web address all by hand
+(defun w3m-open-site (site)
+  "Opens site in new w3m session with 'http://' appended"
+  (interactive
+   (list (read-string "Enter website address(default: w3m-home):" nil nil w3m-home-page nil )))
+  (w3m-goto-url-new-session
+   (concat "http://" site)))
+(provide 'init-browser)
