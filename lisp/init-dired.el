c26fd6ebfcb08f7813932af8ff452c980fd06f6e
sdll <shura.sdll@gmail.com>
2017-01-23 01:12:20 +0000

    fixed dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -15,12 +15,10 @@
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
   (define-key dired-mode-map [mouse-2] 'dired-find-file)
-  (add-hook 'dired-mode-hook
-            (lambda () (guide-key/add-local-guide-key-sequence "%"))))
 
-(when (maybe-require-package 'diff-hl)
-  (after-load 'dired
-    (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
+  (when (maybe-require-package 'diff-hl)
+    (after-load 'dired
+      (add-hook 'dired-mode-hook 'diff-hl-dired-mode))))
 
 (global-set-key (kbd "<f9> d") 'dired)
 
