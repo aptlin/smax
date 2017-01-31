c97f793ad04b87b46ca96c841d645a9af60cd121
sdll <shura.sdll@gmail.com>
2017-01-31 04:27:34 +0000

    fixed navy

--- scimax.el
+++ scimax.el
@@ -147,10 +147,10 @@ This enables you to use tab to open and close outlines."
 (defvar navy-l 'forward-char
   "The next item in a forward sense.")
 
-(defvar navy-j 'backward-char
+(defvar navy-h 'backward-char
   "The previous item in a backward sense.")
 
-(defvar navy-i 'previous-line
+(defvar navy-j 'previous-line
   "The previous item in an up sense.")
 
 (defvar navy-k 'next-line
@@ -171,9 +171,6 @@ This enables you to use tab to open and close outlines."
 (defvar navy-slash 'end-of-visual-line
   "The end of an item.")
 
-(defvar navy-h 'beginning-of-visual-line
-  "Command bound to h, usually a beginning of command.")
-
 (defvar navy-mode "char"
   "The active mode.")
 
@@ -181,21 +178,21 @@ This enables you to use tab to open and close outlines."
 (defhydra navy (:color red :hint nil) 
   "
 %s(format \"%s-mode\" navy-mode)
-%s(make-string (length (symbol-name navy-j)) ? )     _i_: %`navy-i
-%`navy-j :_j_     _l_: %`navy-l     _;_: %`navy-semicolon  _'_: %`navy-quote
-%s(make-string (length (symbol-name navy-j)) ? )     _k_: %`navy-k
+%s(make-string (length (symbol-name navy-h)) ? )     _j_: %`navy-j
+%`navy-h :_j_     _l_: %`navy-l     _;_: %`navy-semicolon  _'_: %`navy-quote
+%s(make-string (length (symbol-name navy-h)) ? )     _k_: %`navy-k
   _,_: %`navy-comma _._: %`navy-period _/_: %`navy-slash
   point-min: _<_    _>_: point-max     
    
 "
-  ("j" (funcall navy-j))
+  ("h" (funcall navy-h))
   ("l" (funcall navy-l))
-  ("i" (funcall navy-i))
+  ("j" (funcall navy-j))
   ("k" (funcall navy-k))
 
   ("q" nil "quit" :color blue)
 
-  ("h" (call-interactively navy-h))
+  ("j" (call-interactively navy-j))
   
   (";" (call-interactively navy-semicolon))
   ("'" (call-interactively navy-quote))
@@ -212,8 +209,8 @@ This enables you to use tab to open and close outlines."
   ("c" (lambda ()
 	 (interactive)
 	 (setq navy-mode "char"
-	       navy-j 'backward-char
-	       navy-i 'previous-line
+	       navy-h 'backward-char
+	       navy-j 'previous-line
 	       navy-l 'forward-char 
 	       navy-k 'next-line
 	       navy-semicolon 'avy-goto-char-2
@@ -225,8 +222,8 @@ This enables you to use tab to open and close outlines."
   ("w" (lambda ()
 	 (interactive)
 	 (setq navy-mode "word"
-	       navy-j 'backward-word
-	       navy-i 'previous-line
+	       navy-h 'backward-word
+	       navy-j 'previous-line
 	       navy-l 'forward-word 
 	       navy-k 'next-
 	       navy-semicolon 'avy-goto-char-2
@@ -238,8 +235,8 @@ This enables you to use tab to open and close outlines."
   ("s" (lambda ()
 	 (interactive)
 	 (setq navy-mode "sentence"
-	       navy-j 'backward-sentence
-	       navy-i 'previous-line
+	       navy-h 'backward-sentence
+	       navy-j 'previous-line
 	       navy-k 'next-line
 	       navy-l 'forward-sentence
 	       navy-semicolon 'avy-goto-char-2
@@ -251,9 +248,9 @@ This enables you to use tab to open and close outlines."
   ("p" (lambda ()
 	 (interactive)
 	 (setq navy-mode "paragraph"
-	       navy-j 'backward-paragraph
+	       navy-h 'backward-paragraph
 	       navy-l 'forward-paragraph
-	       navy-i 'previous-line
+	       navy-j 'previous-line
 	       navy-k 'next-line 
 	       navy-semicolon 'avy-goto-char-2
 	       navy-quote 'avy-goto-line
@@ -264,9 +261,9 @@ This enables you to use tab to open and close outlines."
   ("g" (lambda ()
 	 (interactive)
 	 (setq navy-mode "page"
-	       navy-j 'backward-page
+	       navy-h 'backward-page
 	       navy-l 'forward-page
-	       navy-i 'backward-page
+	       navy-j 'backward-page
 	       navy-k 'forward-page 
 	       navy-semicolon 'avy-goto-char-2
 	       navy-quote 'avy-goto-line
@@ -277,10 +274,10 @@ This enables you to use tab to open and close outlines."
   ("n" (lambda ()
 	 (interactive)
 	 (setq navy-mode "line"
-	       navy-i 'avy-goto-line-above
+	       navy-j 'avy-goto-line-above
 	       navy-k 'avy-goto-line-below
 	       navy-l 'next-line
-	       navy-j 'previous-line 
+	       navy-h 'previous-line 
 	       navy-semicolon 'avy-goto-char-2
 	       navy-quote 'avy-goto-line
 	       navy-comma 'avy-goto-word-1 
@@ -290,9 +287,9 @@ This enables you to use tab to open and close outlines."
   ("x" (lambda ()
 	 (interactive)
 	 (setq navy-mode "sexp"
-	       navy-j 'backward-sexp
+	       navy-h 'backward-sexp
 	       navy-l 'forward-sexp
-	       navy-i 'previous-line
+	       navy-j 'previous-line
 	       navy-k 'next-line
 	       navy-semicolon 'avy-goto-char-2
 	       navy-quote 'avy-goto-line
@@ -310,14 +307,13 @@ This enables you to use tab to open and close outlines."
   "Run the `navy/body' hydra."
   (interactive)
   (setq navy-mode "char"
-	navy-j 'backward-char
-	navy-i 'previous-line
+	navy-h 'backward-char
+	navy-j 'previous-line
 	navy-l 'forward-char 
 	navy-k 'next-line
 	navy-quote 'avy-goto-line
 	navy-comma 'avy-goto-char-2
 	navy-period 'avy-goto-char-in-line
-	navy-h 'beginning-of-visual-line
 	navy-semicolon 'avy-goto-char)
   (navy/body))
 
--- user/init-utils.el
+++ user/init-utils.el
@@ -12,6 +12,20 @@
 (global-set-key (kbd "C-c m p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c m t") 'mc/mark-all-like-this)
+;; extend string editing
+(defun swap-text (str1 str2 beg end)
+  "Changes all STR1 to STR2 and all STR2 to STR1 in beg/end region."
+  (interactive "sString A: \nsString B: \nr")
+  (if mark-active
+      (setq deactivate-mark t)
+    (setq beg (point-min) end (point-max))) 
+  (goto-char beg)
+  (while (re-search-forward
+          (concat "\\(?:\\b\\(" (regexp-quote str1) "\\)\\|\\("
+                  (regexp-quote str2) "\\)\\b\\)") end t)
+    (if (match-string 1)
+	(replace-match str2 t t)
+      (replace-match str1 t t))))
 ;; From active region to multiple cursors:
 (global-set-key (kbd "C-c m r") 'set-rectangular-region-anchor)
 (global-set-key (kbd "C-c m c") 'mc/edit-lines)
