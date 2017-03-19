9fb9064b32e511195af1f2fd4ece74415016df18
sdll <shura.sdll@gmail.com>
2017-03-19 03:59:06 +0000

    fixed the path to the file

--- user/init-locales.el
+++ user/init-locales.el
@@ -1,3 +1,3 @@
-(load-file "~/.emacs.d/user/site-lisp/cyrillic-dvorak.el")
+(load-file (expand-file-name "site-lisp/cyrillic-dvorak.el" user-dir))
 (setq default-input-method "cyrillic-dvorak")
 (provide 'init-locales)
